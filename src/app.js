1b514a190cf4992556bdb135fc3bf936c467e2f8
Md Rasel khan <raselkhanlab@gmail.com>
2024-10-21 12:57:44 +0600

    Initialize application
    Project setup, add required packages and node-express server setup,
    Moongoose connection setup for mongoDb

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const path = require("path");
+const multer = require("multer");
+const mongoose  = require("mongoose");
+const app = express();
+const cors = require("cors");
+const { MONGO_URL } = require('./config/environment');
+
+
+//MongoDb connection
+mongoose.connect(MONGO_URL, {socketTimeoutMS: 1000}).then(()=>{console.log("MongoDB is connected")})
+ .catch((err)=>{console.error(err)});
+
+
+//uploader config
+const storage = multer.diskStorage({
+  destination: '../public',
+  filename(_, file, cb) {
+    cb(null, "google-form-content-questions-" + Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage });
+
+
+//middleware
+app.use(express.static('../public'))
+app.use(cors());
+app.use(bodyParser.json({limit : '50mb',extended : true}));
+app.use(bodyParser.urlencoded({limit: '50mb',extended : true}));
+app.use(express.json());
+
+//Health check route
+app.get("/", (_, res)=> {
+  return res.json({message: "application is running"})
+});
+
+
+module.exports = { app };
---
+++ src/config/environment.js
@@ -0,0 +1,6 @@
+
+exports.PORT  = parseInt(process.env.PORT || 3000);
+
+exports.MONGO_URL = process.env.MONGO_URL;
+
+
