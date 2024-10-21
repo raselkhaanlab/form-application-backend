344e8dcf3a31c826a47041ecc11914e1d0927068
Md Rasel khan <raselkhanlab@gmail.com>
2024-10-21 14:44:55 +0600

    feat: add User model

---
+++ src/model/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  image: {type: String},
+  createdForms: []
+ }, {timestamps: true});
+
+UserSchema.plugin(mongoosePaginate);
+User = mongoose.model('User', UserSchema, 'Users');
+module.exports = User;
