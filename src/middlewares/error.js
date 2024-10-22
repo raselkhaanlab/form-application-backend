dabe8684d8bc343f393c0ee3da4f560cc49835a4
Md Rasel khan <raselkhanlab@gmail.com>
2024-10-22 16:04:19 +0600

    refactor: refactor error middleware
    Send stack trace and error message on dev environment

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,9 +1,12 @@
 
 const { ValidationError } = require("yup");
 const { ApiError } = require("../utils/ApiError");
-
+const isDev = ["development", "dev", "develop"].includes(process.env.NODE_ENV ?? "");
 const errorHandler = (err, _, res, __) => {
   let convertedError = err;
+  if(isDev) {
+    console.error(err);
+  }
   if(convertedError instanceof ValidationError) {
     res.message = convertedError.message || "Validation failed";
     return res.status(400).json(convertedError.errors)
@@ -13,8 +16,9 @@ const errorHandler = (err, _, res, __) => {
     return res.status(convertedError.status).json(null);
   }
   
-  res.message = "Internal server error"
-  return res.status(500).json(null);
+  res.message = isDev? convertedError.message : "Internal server error";
+  const stack = isDev? convertedError.stack : null;
+  return res.status(500).json(stack);
 };
 
 module.exports= {errorHandler};
